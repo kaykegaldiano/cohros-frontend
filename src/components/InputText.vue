6617924f3f35d394ed5eb25945e89c3e38bdcd93
Kayke Galdiano <kaykegaldiano@gmail.com>
2024-01-25 00:36:53 -0300

    wip register view

---
+++ src/components/InputText.vue
@@ -0,0 +1,21 @@
+<script setup>
+    defineProps({
+        name: {
+            type: String,
+            required: true
+        },
+        lblText: {
+            type: String,
+            required: true
+        },
+        disabled: Boolean,
+        required: Boolean
+    })
+</script>
+
+<template>
+    <label :for="name" class="block text-sm font-medium leading-6 text-gray-900">{{ lblText }}</label>
+        <div class="mt-2">
+            <input :id="name" :name="name" :disabled="disabled" :required="required" type="text" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+        </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue') 
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
